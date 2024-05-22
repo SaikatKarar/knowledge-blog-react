63e612c21950cf694dd31faebda6091110fc0d25
SaikatKarar <kararsaikat@gmail.com>
2024-05-22 20:46:44 +0530

    all resources

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './component/Navbar/Navbar'
+import Card from './component/Card/Card';
+import Sidebar from './component/Sidebar/Sidebar';
+import Blog from './component/Blog/Blog';
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Card />
+      <Sidebar />
+      <Blog />
+    </>
+  )
+}
+
+export default App
